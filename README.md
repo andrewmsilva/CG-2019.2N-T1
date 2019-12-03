aaab4d76e9b3d18f00c6129c2ef2ec308c0bf748
Andrew M. Silva <andrewsaxx@gmail.com>
2019-12-03 08:41:17 -0300

    Example of Pokemon loading

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Pokemon-Animation
+# Models:
+[\[ 1 \] - Pokémon XY FBX](https://www.models-resource.com/3ds/pokemonxy/)
+
+### HOW TO
+
+[\[ 1 \] - FBX three js](https://threejs.org/examples/?q=fbx#webgl_loader_fbx)
+
+[\[ 2 \] - Loading FBX (example)](https://github.com/mrdoob/three.js/blob/master/examples/webgl_loader_fbx.html)
+
+[\[ 3 \] - Loading texture (example)](https://stackoverflow.com/questions/53019373/fbx-loader-in-three-js)
+
+[\[ 4 \] - Based repo](https://github.com/oraphaBorges/CG-2019.2N-T1)
---
+++ load-fbx.js
@@ -0,0 +1,61 @@
+import * as THREE from './libs/js/three.js/build/three.module.js';
+import Stats from './libs/js/three.js/examples/jsm/libs/stats.module.js';
+
+import { OrbitControls } from './libs/js/three.js/examples/jsm/controls/OrbitControls.js';
+import { FBXLoader } from './libs/js/three.js/examples/jsm/loaders/FBXLoader.js';
+
+var container, stats, controls;
+var camera, scene, renderer, mixer;
+
+var clock = new THREE.Clock();
+
+init();
+animate();
+
+function init(){
+    // Render localization
+    container = document.createElement('div');
+    document.body.appendChild( container );
+
+    // Camera ajusts
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 2000 );
+    camera.position.set( 100, 200, 400 );
+
+    // Create scene
+    scene = new THREE.Scene();
+
+    // Load Model
+    var loader = new FBXLoader();
+    loader.load( './src/models/fbx/Arcanine/Arcanine.FBX', function ( object ) {
+        var texture = new THREE.TextureLoader().load('./src/models/fbx/Arcanine/images/pm0059_00_BodyA1.png');
+        object.traverse( function ( child ) {
+            if ( child.isMesh ) {
+                child.material = new THREE.MeshBasicMaterial( { map: texture } )
+            }
+        } );
+        scene.add( object );
+    } );
+
+    // Render Model
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.shadowMap.enabled = true;
+
+    // Include model on Render localization
+    container.appendChild( renderer.domElement);
+
+    // Turn on the controls
+    controls = new OrbitControls( camera, renderer.domElement );
+    controls.target.set( 0, 100, 0 );
+    controls.update();
+}
+
+// Function to run all render
+function animate() {
+    requestAnimationFrame( animate );
+    var delta = clock.getDelta();
+    if ( mixer ) mixer.update( delta );
+    renderer.render( scene, camera );
+    stats.update();
+}
